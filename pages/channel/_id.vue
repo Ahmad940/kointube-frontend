1a99b50e176880d25f92c090ef8650034f94590d
ahmad940 <ahmadmuhammadmak5@gmail.com>
2021-10-23 03:48:50 +0100

    added progress bar for channel image

--- pages/channel/_id.vue
+++ pages/channel/_id.vue
@@ -15,7 +15,20 @@
             height="250"
             :src="video.thumbnailUrl"
             :alt="video.title"
-          ></v-img>
+          >
+            <template #placeholder>
+              <v-row
+                class='fill-height ma-0'
+                align='center'
+                justify='center'
+              >
+                <v-progress-circular
+                  indeterminate
+                  color='grey lighten-5'
+                ></v-progress-circular>
+              </v-row>
+            </template>
+          </v-img>
 
 <!--          <v-card-text>-->
 <!--          </v-card-text>-->
